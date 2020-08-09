64307793699c08334518e226e7fb54a9a62d31b0
Chris George <chrisg1622@gmail.com>
2020-08-09 15:46:39 +0100

    added tensorflow settings

---
+++ tensorflow.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+function tb { check_project 'tb' && tmux new -d -s tb 'sa && tensorboard --logdir=${PROJECT_DIR}/tensorboard' }
+alias tbl="tmux ls"
+alias tbkill="tmux kill-session -t tb"
+function tbclear { check_project 'tbclear' && rm ${PROJECT_DIR}/tensorboard/* }
